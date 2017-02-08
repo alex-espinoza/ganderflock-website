e051f279c415652e46656e1f67c7ff36065b8cad
Alex Espinoza <aespinoza@aspria.net>
2017-02-08 11:10:21 -0500

    Add setTimeout before removing body .no-scroll class as overflow:hidden messes up section transitions in firefox

--- js/script.js
+++ js/script.js
@@ -53,25 +53,37 @@ var animationEndEvent = whichAnimationEndEvent();
 topLeftBlock.addEventListener("click", function() {
   blockContainer.classList.add("section-transition-top-left");
   webDesignSection.classList.add("active");
-  bodyElement.classList.remove("no-scroll");
+
+  setTimeout(function() {
+    bodyElement.classList.remove("no-scroll");
+  }, 1000);
 });
 
 topRightBlock.addEventListener("click", function() {
   blockContainer.classList.add("section-transition-top-right");
   creativeSection.classList.add("active");
-  bodyElement.classList.remove("no-scroll");
+
+  setTimeout(function() {
+    bodyElement.classList.remove("no-scroll");
+  }, 1000);
 });
 
 bottomLeftBlock.addEventListener("click", function() {
   blockContainer.classList.add("section-transition-bottom-left");
   socialMediaSection.classList.add("active");
-  bodyElement.classList.remove("no-scroll");
+
+  setTimeout(function() {
+    bodyElement.classList.remove("no-scroll");
+  }, 1000);
 });
 
 bottomRightBlock.addEventListener("click", function() {
   blockContainer.classList.add("section-transition-bottom-right");
   teamSection.classList.add("active");
-  bodyElement.classList.remove("no-scroll");
+
+  setTimeout(function() {
+    bodyElement.classList.remove("no-scroll");
+  }, 1000);
 });
 
 goBackLogoElements.forEach(function(logoElement) {
@@ -90,19 +102,60 @@ goBackLogoElements.forEach(function(logoElement) {
 
 // ScrollMagic
 
+var webDesignSectionHeroVideoElement = document.querySelector("#web-design-section .hero-video");
+var webDesignSectionImageOneElement = document.querySelector("#web-design-section .images-container .image.one");
+var webDesignSectionImageTwoElement = document.querySelector("#web-design-section .images-container .image.two");
+var webDesignSectionImageThreeElement = document.querySelector("#web-design-section .images-container .image.three");
+var webDesignSectionImageFourElement = document.querySelector("#web-design-section .images-container .image.four");
+
 var creativeSectionHeroVideoElement = document.querySelector("#creative-section .hero-video");
 var creativeSectionImageOneElement = document.querySelector("#creative-section .images-container .image.one");
 var creativeSectionImageTwoElement = document.querySelector("#creative-section .images-container .image.two");
 var creativeSectionImageThreeElement = document.querySelector("#creative-section .images-container .image.three");
 var creativeSectionImageFourElement = document.querySelector("#creative-section .images-container .image.four");
 
-var tweenTimeline = new TimelineMax();
-tweenTimeline.add([
+var socialMediaSectionHeroVideoElement = document.querySelector("#social-media-section .hero-video");
+var socialMediaSectionImageOneElement = document.querySelector("#social-media-section .images-container .image.one");
+var socialMediaSectionImageTwoElement = document.querySelector("#social-media-section .images-container .image.two");
+var socialMediaSectionImageThreeElement = document.querySelector("#social-media-section .images-container .image.three");
+var socialMediaSectionImageFourElement = document.querySelector("#social-media-section .images-container .image.four");
+
+var teamSectionHeroVideoElement = document.querySelector("#team-section .hero-video");
+var teamSectionImageOneElement = document.querySelector("#team-section .images-container .image.one");
+var teamSectionImageTwoElement = document.querySelector("#team-section .images-container .image.two");
+var teamSectionImageThreeElement = document.querySelector("#team-section .images-container .image.three");
+var teamSectionImageFourElement = document.querySelector("#team-section .images-container .image.four");
+
+var webDesignSectionTweenTimeline = new TimelineMax();
+webDesignSectionTweenTimeline.add([
+  TweenMax.to(webDesignSectionImageOneElement, 1, {y:-200}),
+  TweenMax.to(webDesignSectionImageTwoElement, 1, {y:-150}),
+  TweenMax.to(webDesignSectionImageThreeElement, 1, {y:-200}),
+  TweenMax.to(webDesignSectionImageFourElement, 1, {y:-150}),
+]);
+
+var creativeSectionTweenTimeline = new TimelineMax();
+creativeSectionTweenTimeline.add([
   TweenMax.to(creativeSectionImageOneElement, 1, {y:-200}),
   TweenMax.to(creativeSectionImageTwoElement, 1, {y:-150}),
   TweenMax.to(creativeSectionImageThreeElement, 1, {y:-200}),
   TweenMax.to(creativeSectionImageFourElement, 1, {y:-150}),
-  // TweenMax.to(creativeSectionImagesMoveLessElements, 1, {y:-150})
+]);
+
+var socialMediaSectionTweenTimeline = new TimelineMax();
+socialMediaSectionTweenTimeline.add([
+  TweenMax.to(socialMediaSectionImageOneElement, 1, {y:-200}),
+  TweenMax.to(socialMediaSectionImageTwoElement, 1, {y:-150}),
+  TweenMax.to(socialMediaSectionImageThreeElement, 1, {y:-200}),
+  TweenMax.to(socialMediaSectionImageFourElement, 1, {y:-150}),
+]);
+
+var teamSectionTweenTimeline = new TimelineMax();
+teamSectionTweenTimeline.add([
+  TweenMax.to(teamSectionImageOneElement, 1, {y:-200}),
+  TweenMax.to(teamSectionImageTwoElement, 1, {y:-150}),
+  TweenMax.to(teamSectionImageThreeElement, 1, {y:-200}),
+  TweenMax.to(teamSectionImageFourElement, 1, {y:-150}),
 ]);
 
 var controller = new ScrollMagic.Controller({
@@ -113,10 +166,34 @@ var controller = new ScrollMagic.Controller({
 });
 
 new ScrollMagic.Scene({
+  triggerElement: webDesignSectionHeroVideoElement,
+  duration: 700
+})
+.setTween(webDesignSectionTweenTimeline)
+// .addIndicators({name: "image parallax"})
+.addTo(controller);
+
+new ScrollMagic.Scene({
   triggerElement: creativeSectionHeroVideoElement,
   duration: 700
 })
-.setTween(tweenTimeline)
+.setTween(creativeSectionTweenTimeline)
+// .addIndicators({name: "image parallax"})
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: socialMediaSectionHeroVideoElement,
+  duration: 700
+})
+.setTween(socialMediaSectionTweenTimeline)
+// .addIndicators({name: "image parallax"})
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: teamSectionHeroVideoElement,
+  duration: 700
+})
+.setTween(teamSectionTweenTimeline)
 // .addIndicators({name: "image parallax"})
 .addTo(controller);
 
